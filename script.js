d2dc31d7dc2bd71158c6487804c0c6977e0f0a62
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-04-04 20:03:06 +0100

    created post request for data from server for website. made the feeback section more visible on mobile phones

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // -- Global -- 
 
 const MAX_CHARS = 150;
+const BASE_API_URL = 'https://bytegrad.com/course-assets/js/1/api';
 
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
@@ -10,27 +11,27 @@ const submitBtnEl = document.querySelector('.submit-btn');
 const spinnerEl = document.querySelector('.spinner');
 
 const renderFeedbackItem = feedbackItem => {
-       // new feedback item html
-       const feedbackItemHtml =
-       `<li class="feedback">
-       <button class="upvote">
-           <i class="fa-solid fa-caret-up upvote__icon"></i>
-           <span class="upvote__count">${feedbackItem.upvoteCount}</span>
-       </button>
-       <section class="feedback__badge">
-           <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
-       </section>
-       <div class="feedback__content">
-           <p class="feedback__company">${feedbackItem.company}</p>
-           <p class="feedback__text">${feedbackItem.text}</p>
-       </div>
-       <p class="feedback__date">${feedbackItem.daysAgo === 0 ? 'NEW' : `${feedbackItem.daysAgo}d`}</p>
-   </li>`;
+    // new feedback item html
+    const feedbackItemHtml =
+        `<li class="feedback">
+            <button class="upvote">
+                <i class="fa-solid fa-caret-up upvote__icon"></i>
+                <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+            </button>
+            <section class="feedback__badge">
+                <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+            </section>
+            <div class="feedback__content">
+                <p class="feedback__company">${feedbackItem.company}</p>
+                <p class="feedback__text">${feedbackItem.text}</p>
+            </div>
+            <p class="feedback__date">${feedbackItem.daysAgo === 0 ? 'NEW' : `${feedbackItem.daysAgo}d`}</p>
+        </li>`;
 
 
-   //insert new feedback item in list
-//    feedbackListEl.insertAdjacentHTML('beforebegin', feedbackItemHtml);
-     feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHtml);
+    //insert new feedback item in list
+    // feedbackListEl.insertAdjacentHTML('beforebegin', feedbackItemHtml);
+    feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHtml);
 
 }
 
@@ -54,9 +55,7 @@ textareaEl.addEventListener('input', inputHandler);
 
 
 const showVisualIndicator = textCheck => {
-
     const className = textCheck === 'valid' ? 'form--valid' : 'form--invalid';
-
 
     //show vaild indicator
     formEl.classList.add(className);
@@ -75,7 +74,6 @@ const submitHandler = (event) => {
     event.preventDefault();
     const text = textareaEl.value;
 
-
     //validate text(check length of text and #hashtag is present)
     if (text.includes('#') && text.length > 4) {
         showVisualIndicator('valid');
@@ -85,7 +83,6 @@ const submitHandler = (event) => {
 
         //focus textarea
         textareaEl.focus();
-
         return;
     }
 
@@ -100,17 +97,30 @@ const submitHandler = (event) => {
 
     //feedback item object
     const feedbackItem = {
-        upvoteCount:upvoteCount,
-        company:company,
-        badgeLetter:badgeLetter,
-        daysAgo:daysAgo,
-        text:text,
-
+        upvoteCount: upvoteCount,
+        company: company,
+        badgeLetter: badgeLetter,
+        daysAgo: daysAgo,
+        text: text,
     }
 
     //render feedback item
-    renderFeedbackItem(feedbackItem); 
- 
+    renderFeedbackItem(feedbackItem);
+
+    fetch(`${BASE_API_URL}/feedbacks`, {
+            method: 'POST',
+            body: JSON.stringify(feedbackItem),
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            if(!response.ok){
+                console.log('Something went wrong');
+                return;
+            }
+            console.log('Successfully submitted');
+        }).catch(error => console.log(error));
 
     //clear textarea
     textareaEl.value = '';
@@ -120,18 +130,16 @@ const submitHandler = (event) => {
 
     //reset counter
     counterEl.textContent = MAX_CHARS;
-
 }
 
 formEl.addEventListener('submit', submitHandler);
 
 
-fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks')
+fetch(`${BASE_API_URL}/feedbacks`)
     .then(response => response.json())
     .then(data => {
 
         //remove spinner 
-
         spinnerEl.remove();
 
         // iterate over each element in feedbacks array and render it in list
@@ -140,7 +148,7 @@ fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks')
         });
 
     })
-    .catch(error =>{
+    .catch(error => {
         feedbackListEl.textContent = `Failed to Fetch feedback iterms. Error message ${error.message}`;
     })
 
